308a9e7af63a4cbcbf45340f0d7f6176abbc6cd7
JetOU <31071116+JetOU777@users.noreply.github.com>
2018-09-11 14:30:05 +0300

    Optimize line break-finder regex

--- Compatibilizer.js
+++ Compatibilizer.js
@@ -1,5 +1,5 @@
 function Compatibilizer() {
     var str = document.getElementById("playerlist").value;
-    var res = str.replace(/ VS /g, "\r\n").replace(/^ +/gm, '').replace(/Bye #\d+/g, "").replace(/\n\n/g,'\n');
+    var res = str.replace(/ VS /g, "\r\n").replace(/^ +/gm, '').replace(/Bye #\d+/g, "").replace(/\n\s*\n/gm,'\n');
     document.getElementById("playerlist").value = res;
 }
