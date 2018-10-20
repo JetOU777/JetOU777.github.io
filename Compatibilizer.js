753a9c454461adafaeaec8bef032a7f28c20c33a
JetOU <31071116+JetOU777@users.noreply.github.com>
2018-10-20 20:53:25 +0300

    Be compatible with JSLint

--- Compatibilizer.js
+++ Compatibilizer.js
@@ -1,9 +1,9 @@
 "use strict";
 function Compatibilizer() {
     let playerlist = document.getElementById("playerlist").value;
-    let remove_vs = playerlist.replace(/ VS /g, "\r\n");
-    let remove_extra_spaces = remove_vs.replace(/^ +/gm, '');
-    let remove_byes = remove_extra_spaces.replace(/Bye #\d+/g, "")
-    let remove_extra_linebreaks = remove_byes.replace(/\n\s*\n/gm,'\n');
+    let remove_vs = playerlist.replace(/\sVS\s/g, "\r\n");
+    let remove_extra_spaces = remove_vs.replace(/^\s+/gm, "");
+    let remove_byes = remove_extra_spaces.replace(/Bye\s#\d+/g, "");
+    let remove_extra_linebreaks = remove_byes.replace(/\n\s*\n/gm,"\n");
     document.getElementById("playerlist").value = remove_extra_linebreaks;
 }
