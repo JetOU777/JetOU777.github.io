06c67ac559d184a88e0d75b90a7c443dde9efa21
JetOU777 <waleed.2004.hassan@gmail.com>
2019-03-21 16:01:55 +0300

    Add getStourPoints

---
+++ getStourPoints.js
@@ -0,0 +1,54 @@
+function Compatibilizer(playerlist) {
+    let remove_round = playerlist.replace(/Round\s\d|Finals/g, '');
+    let remove_vs = remove_round.replace(/\svs\.\s/g, '\n');
+    let remove_extra_spaces = remove_vs.replace(/^\s+/gm, '');
+    let remove_byes = remove_extra_spaces.replace(/Bye\s#\d+/g, '');
+    let remove_extra_linebreaks = remove_byes.replace(/\n\s*\n/gm,'\n');
+	return remove_extra_linebreaks.split('\n');
+}
+
+function findDuplicates() {
+	const list = document.getElementById('list').value;
+    let output = `${count(Compatibilizer(`${list}`))}`;
+    document.write(output);	
+}
+
+function count(array_elements) {
+    array_elements.sort();
+	let output = '';
+    let current = null;
+    let cnt = 0;
+    for (let i = 0; i <= array_elements.length; i++) {
+        if (array_elements[i] != current) {
+            switch (cnt) {
+                case 2: case 3: 
+                    output += `${current} recieved ${cnt - 1} points<br />`; 
+                    break;
+                case 4:
+                    output += `${current} recieved ${cnt - 1} points<br />`; 
+                    break;
+                case 5:
+                    output += `${current} recieved ${cnt + 1} points<br />`; 
+                    break;
+                case 6:
+                    output += `${current} recieved ${cnt + 2} points<br />`; 
+                    break;
+                case 7:
+                    output += `${current} recieved ${cnt + 3} points<br />`; 
+                    break;
+                case 8:
+                    output += `${current} recieved ${cnt + 4} points<br />`; 
+                    break;
+                case 9:
+                    output += `${current} recieved ${cnt + 5} points<br />`; 
+                    break;
+            }
+            current = array_elements[i];
+            cnt = 1;
+        } else {
+            cnt++;
+        }
+    }
+    return output;
+}
+
