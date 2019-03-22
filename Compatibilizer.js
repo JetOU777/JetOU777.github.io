bf34d424d92d719e61b773c3e5da0404016311f1
JetOU777 <waleed.2004.hassan@gmail.com>
2019-03-22 16:08:08 +0300

    Improve Compatibilizer's code

--- Compatibilizer.js
+++ Compatibilizer.js
@@ -1,9 +1,11 @@
 "use strict";
 function Compatibilizer() {
     let playerlist = document.getElementById("playerlist").value;
-    let remove_vs = playerlist.replace(/\sVS\s/g, "\r\n");
-    let remove_extra_spaces = remove_vs.replace(/^\s+/gm, "");
-    let remove_byes = remove_extra_spaces.replace(/Bye\s#\d+/g, "");
-    let remove_extra_linebreaks = remove_byes.replace(/\n\s*\n/gm,"\n");
-    document.getElementById("playerlist").value = remove_extra_linebreaks;
+    let output = [];
+    for (let matchup of playerlist.split('\n').map(x => x.split(' VS '))) {
+        if (!matchup || matchup === '\n') continue;
+        const [p1, p2] = matchup;
+        output.push(p1, p2);
+    }
+    document.getElementById("playerlist").value = output.join('\n');
 }
