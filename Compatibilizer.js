52ec9326e82a46c0f263d844083f7cc289b3957c
JetOU777 <waleed.2004.hassan@gmail.com>
2019-03-28 11:06:36 +0300

    Support other formats

--- Compatibilizer.js
+++ Compatibilizer.js
@@ -1,8 +1,9 @@
 "use strict";
 function Compatibilize() {
     let playerlist = document.getElementById("playerlist").value;
+    let remove_vs = playerlist.replace(/vs.?/gi, '');
     let output = [];
-    for (let matchup of playerlist.split('\n').map(x => x.split(' VS '))) {
+    for (let matchup of remove_vs) {
         if (matchup.length < 2) continue;
         const [p1, p2] = matchup;
         !/Bye #\d+/.test(p1) ? output.push(p1) : '';
