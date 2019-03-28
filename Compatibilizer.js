3c82f221c87ff91fbea855ef8e95abc8e2ec1ab2
JetOU777 <waleed.2004.hassan@gmail.com>
2019-03-28 11:25:55 +0300

    Revert past commits

--- Compatibilizer.js
+++ Compatibilizer.js
@@ -1,9 +1,9 @@
 "use strict";
 function Compatibilize() {
     let playerlist = document.getElementById("playerlist").value;
-    let remove_vs = playerlist.replace(/vs.?/gi, '');
     let output = [];
-    for (let matchup of remove_vs) {
+    for (let matchup of playerlist.split('\n').map(x => x.split(' VS '))) {
+        if (matchup.length < 2) continue;
         const [p1, p2] = matchup;
         !/Bye #\d+/.test(p1) ? output.push(p1) : '';
         !/Bye #\d+/.test(p2) ? output.push(p2) : '';
